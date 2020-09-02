99f33f5aa8c6712ea0b78b10f1b444d67aee1db5
Jonny K <Kempj2.jk@gmail.com>
2020-09-02 13:03:57 -0700

    deleting the junk

--- data/Capital_2.js
+++
@@ -1,8 +0,0 @@
-var json_Capital_2 = {
-"type": "FeatureCollection",
-"name": "Capital_2",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "scalerank": 0.0, "featurecla": "Admin-0 capital", "name": "Washington, D.C.", "nameascii": "Washington, D.C.", "adm0cap": 1.0, "sov0name": "United States", "latitude": 38.899549376499998, "longitude": -77.009418580800002, "pop_max": 4338000.0, "pop_min": 552433.0 }, "geometry": { "type": "Point", "coordinates": [ -77.011364439437159, 38.901495235086998 ] } }
-]
-}
